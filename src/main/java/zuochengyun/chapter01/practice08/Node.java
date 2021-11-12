f18846766b62ea84433beb7957cd907ff6164e23
wangyinghao.0609 <wangyinghao.0609@bytedance.com>
2021-11-12 20:48:20 +0800

    20211112
    Change-Id: I6356370b83fdadd81c31efb01112c802bf8f8de8

---
+++ src/main/java/zuochengyun/chapter01/practice08/Node.java
@@ -0,0 +1,11 @@
+package zuochengyun.chapter01.practice08;
+
+public class Node {
+    public int value;
+    public Node left;
+    public Node right;
+
+    public Node(int data) {
+        this.value = data;
+    }
+}
