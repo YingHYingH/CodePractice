05779762a7d79279ea56b6a494a9f7fd2b86dcfa
wangyinghao.0609 <wangyinghao.0609@bytedance.com>
2021-11-24 21:59:05 +0800

    20211124
    Change-Id: Ic865be02017fb796320284748d0c39b62e851fe0

---
+++ src/main/java/zuochengyun/chapter02/practice08/Node.java
@@ -0,0 +1,106 @@
+package zuochengyun.chapter02.practice08;
+
+import com.sun.xml.internal.bind.v2.model.core.EnumLeafInfo;
+
+public class Node {
+    public int value;
+    public Node next;
+
+    public Node(int data) {
+        this.value = data;
+    }
+
+    public Node listPartition1(Node head, int pivot) {
+        if (head == null) {
+            return head;
+        }
+        Node cur = head;
+        int i = 0;
+        while (cur != null) {
+            i++;
+            cur = cur.next;
+        }
+        Node[] nodeArr = new Node[i];
+        cur = head;
+        for (i = 0; i < nodeArr.length; i++) {
+            nodeArr[i] = cur;
+            cur = cur.next;
+        }
+
+        for (i = 1; i < nodeArr.length; i++) {
+            nodeArr[i - 1].next = nodeArr[i];
+        }
+        nodeArr[i - 1].next = null;
+        return nodeArr[0];
+    }
+
+    public void arrPartition(Node[] nodeArr, int pivot) {
+        int small = -1;
+        int big = nodeArr.length;
+        int index = 0;
+        while (index != big) {
+            if (nodeArr[index].value < pivot) {
+                swap(nodeArr, ++small, index++);
+            } else if (nodeArr[index].value == pivot) {
+                index++;
+            } else {
+                swap(nodeArr, --big, index);
+            }
+        }
+    }
+
+    public void swap(Node[] nodeArr, int a, int b) {
+        Node tmp = nodeArr[a];
+        nodeArr[a] = nodeArr[b];
+        nodeArr[b] = tmp;
+    }
+
+    public Node listPartition2(Node head, int pivot) {
+        Node sH = null;//小头
+        Node sT = null;//小尾
+        Node eH = null;
+        Node eT = null;
+        Node bH = null;
+        Node bT = null;
+        Node next = null;
+        // 所有的节点分进三个链表中
+        while (head != null) {
+            next = head.next;
+            head.next = null;
+            if (head.value < pivot) {
+                if (sH == null) {
+                    sH = head;
+                    sT = head;
+                } else {
+                    sT.next = head;
+                    sT = head;
+                }
+            } else if (head.value == pivot) {
+                if (eH == null) {
+                    eH = head;
+                    eT = head;
+                } else {
+                    eT.next = head;
+                    eT = head;
+                }
+            } else {
+                if (bH == null) {
+                    bH = head;
+                    bT = head;
+                } else {
+                    bT.next = head;
+                    bT = head;
+                }
+            }
+            head = next;
+        }
+        if (sT != null) {
+            sT.next = eH;
+            eT = eT == null ? sT : eT;
+        }
+        if (eT != null) {
+            eT.next = bH;
+        }
+        return sH != null ? sH : eH != null ? eH : bH;
+    }
+}
