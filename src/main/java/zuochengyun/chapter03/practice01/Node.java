ffc1fdad474ef76a72502ce54d8b15d389f9550b
wangyinghao.0609 <wangyinghao.0609@bytedance.com>
2021-12-07 21:45:19 +0800

    20211207
    Change-Id: Idc4a4132e318d161b7896993d2a6feb327b04bdd

---
+++ src/main/java/zuochengyun/chapter03/practice01/Node.java
@@ -0,0 +1,118 @@
+package zuochengyun.chapter03.practice01;
+
+import java.util.Stack;
+
+public class Node {
+    public int value;
+    public Node left;
+    public Node right;
+
+    public Node(int data) {
+        this.value = data;
+    }
+
+    public void preOrderRecur(Node head) {
+        if (head == null) {
+            return;
+        }
+        System.out.println(head.value + " ");
+        preOrderRecur(head.left);
+        preOrderRecur(head.right);
+    }
+
+    public void inOrderRecur(Node head) {
+        if (head == null) {
+            return;
+        }
+        inOrderRecur(head.left);
+        System.out.println(head.value + " ");
+        inOrderRecur(head.right);
+    }
+
+    public void posOrderRecur(Node head) {
+        if (head == null) {
+            return;
+        }
+        posOrderRecur(head.left);
+        posOrderRecur(head.right);
+        System.out.println(head.value + " ");
+    }
+
+    public void preOrderUnRecur(Node head) {
+        System.out.println("pre-order: ");
+        if (head != null) {
+            Stack<Node> stack = new Stack<>();
+            stack.push(head);
+            while (!stack.isEmpty()) {
+                head = stack.pop();
+                System.out.println(head.value + " ");
+                if (head.right != null) {
+                    stack.push(head.right);
+                }
+                if (head.left != null) {
+                    stack.push(head.left);
+                }
+            }
+        }
+    }
+
+    public void inOrderUnRecur(Node head) {
+        System.out.println("in-order: ");
+        if (head != null) {
+            Stack<Node> stack = new Stack<>();
+            while (!stack.isEmpty() || head != null) {
+                if (head != null) {
+                    stack.push(head);
+                    head = head.left;
+                } else {
+                    head = stack.pop();
+                    System.out.println(head.value + " ");
+                    head = head.right;
+                }
+            }
+        }
+    }
+
+    public void posOrderUnRecur1(Node head) {
+        System.out.println("pos-order: ");
+        if (head != null) {
+            Stack<Node> s1 = new Stack<>();
+            Stack<Node> s2 = new Stack<>();
+            s1.push(head);
+            while (!s1.isEmpty()) {
+                head = s1.pop();
+                s2.push(head);
+                if (head.left != null) {
+                    s1.push(head.left);
+                }
+                if (head.right != null) {
+                    s1.push(head.right);
+                }
+            }
+            while (!s2.isEmpty()) {
+                System.out.println(s2.pop().value + " ");
+            }
+        }
+    }
+
+    public void posOrderUnRecur2(Node head) {
+        System.out.println("pos-order: ");
+        if (head != null) {
+            Stack<Node> stack = new Stack<>();
+            stack.push(head);
+            // c是当前stack的栈顶节点 head代表最近一次弹出并打印的节点
+            Node c = null;
+            while (!stack.isEmpty()) {
+                c = stack.peek();
+                if (c.left != null && head != c.left && head != c.right) {
+                    stack.push(c.left);
+                } else if (c.right != null && head != c.right) {
+                    stack.push(c.right);
+                } else {
+                    System.out.println(stack.pop().value + " ");
+                    head = c;
+                }
+            }
+        }
+    }
+}
