4022b74c3c81e04fd15121dafef3ef3c79aaedae
wangyinghao.0609 <wangyinghao.0609@bytedance.com>
2021-12-20 22:06:07 +0800

    20211220
    Change-Id: I5e83c41819d7dacc8c103b79f6586b8adea370b3

---
+++ src/main/java/zuochengyun/chapter05/practice01/Solution.java
@@ -0,0 +1,21 @@
+package zuochengyun.chapter05.practice01;
+
+public class Solution {
+    public boolean isDeformation(String str1, String str2) {
+        if (str1 == null || str2 == null || str1.length() != str2.length()) {
+            return false;
+        }
+        char[] chas1 = str1.toCharArray();
+        char[] chas2 = str2.toCharArray();
+        int[] map = new int[256];
+        for (int i = 0; i < chas1.length; i++) {
+            map[chas1[i]]++;
+        }
+        for (int i = 0; i < chas2.length; i++) {
+            if (map[chas2[i]--] == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
